c3af5d603c5570a13e64312f6b56f325fc99b7f3
Andrzej Broński <andrzej1_1@o2.pl>
2026-01-03 17:26:11 +0100

    Add support for r0vm on macOS.

--- pkgs/r0vm/default.nix
+++ pkgs/r0vm/default.nix
@@ -1,4 +1,5 @@
-{ stdenv
+{ lib
+, stdenv
 , fetchurl
 , autoPatchelfHook
 }:
@@ -8,16 +9,27 @@ stdenv.mkDerivation rec {
   # NOTE: You can find the latest version at https://github.com/risc0/risc0/releases/latest.
   version = "3.0.4";
 
-  src = fetchurl {
-    url = "https://github.com/risc0/risc0/releases/download/v${version}/cargo-risczero-x86_64-unknown-linux-gnu.tgz";
-    hash = "sha256-Oyn7XrE8/yVbL/4PbnlirEqq9q9ZcFYn4oUfHbH6eC8=";
-  };
+  src = fetchurl (
+    {
+      x86_64-linux = {
+        url = "https://github.com/risc0/risc0/releases/download/v${version}/cargo-risczero-x86_64-unknown-linux-gnu.tgz";
+        hash = "sha256-Oyn7XrE8/yVbL/4PbnlirEqq9q9ZcFYn4oUfHbH6eC8=";
+      };
+      aarch64-darwin = {
+        url = "https://github.com/risc0/risc0/releases/download/v${version}/cargo-risczero-aarch64-apple-darwin.tgz";
+        hash = "sha256-9tSEbVzWouUWUFm1ZvmjH4eVgrwlNLlQFan0BNObGHU=";
+      };
+    }.${stdenv.hostPlatform.system} or (throw "Unsupported platform: ${stdenv.hostPlatform.system}")
+  );
 
   sourceRoot = ".";
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = lib.optionals stdenv.isLinux [ autoPatchelfHook ];
 
-  buildInputs = [ stdenv.cc.cc.lib ];
+  buildInputs = lib.optionals stdenv.isLinux [
+    # Required for ELF-patching libstdc++.so (C++ standard library) and libgcc_s.so (GCC runtime library).
+    stdenv.cc.cc.lib
+  ];
 
   installPhase = ''
     runHook preInstall
