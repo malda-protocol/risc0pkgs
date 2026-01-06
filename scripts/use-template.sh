259df36c42a5ba2589d57bd52cc18bcab89a4a79
Andrzej Broński <andrzej1_1@o2.pl>
2026-01-06 11:02:12 +0100

    Add template check in CI.

--- scripts/use-template.sh
+++ scripts/use-template.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-mkdir risc0-workspace
+# Create a temporary workspace and test the template
+mkdir -p risc0-workspace
 cd risc0-workspace
 
+# Initialize from template
 nix flake init -t ..#default
 
+# Git is required for flake to work with local files
 git init
 git add -A
 
-# Override the nixpkgs input of the template to use the current branch,
-# since that is the branch of interest we want to test
-nix flake lock --override-input risc0pkgs "github:malda-protocol/risc0pkgs/$GITHUB_REF"
+# Override risc0pkgs input to use current branch/commit
+nix flake lock --override-input risc0pkgs "github:malda-protocol/risc0pkgs/${GITHUB_REF:-master}"
 
-nix build --accept-flake-config -L --no-link .#risc0package
-
+# Build the template
+nix build --accept-flake-config -L --no-link .#default
