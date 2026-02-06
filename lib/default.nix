b216ca7822def8f572f8c883cfd5c73d5fb367d4
Andrzej Broński <andrzej1_1@o2.pl>
2026-02-06 12:28:14 +0100

    Remove `buildRisc0Package` - we build guest separately.

--- lib/default.nix
+++ lib/default.nix
@@ -2,9 +2,6 @@
 
 lib.makeScope pkgs.newScope (
   self: with self; {
-    buildRisc0Package = callPackage ./buildRisc0Package.nix {
-      inherit (pkgs) r0vm risc0-rust;
-    };
     buildRisc0Guest = callPackage ./buildRisc0Guest.nix {
       inherit (pkgs) r0vm risc0-rust;
     };
--- overlay.nix
+++ overlay.nix
@@ -5,7 +5,6 @@ final: prev: {
   # Cross-compiler for guest C/C++ code (riscv32im target)
   riscv32-cc = final.pkgsCross.riscv32-embedded.stdenv.cc;
 
-  buildRisc0Package = final.callPackage ./lib/buildRisc0Package.nix { };
   buildRisc0Guest = final.callPackage ./lib/buildRisc0Guest.nix { };
   buildRisc0Host = final.callPackage ./lib/buildRisc0Host.nix { };
 }
